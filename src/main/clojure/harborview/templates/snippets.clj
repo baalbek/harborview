c011f6cb2bd5d9a9f174bea682d9f6280944c939
rcs <rolf.c.stadheim@gmail.com>
2015-11-21 15:46:58 +0100

    index.html with head and div.scripts

--- src/main/clojure/harborview/templates/snippets.clj
+++ src/main/clojure/harborview/templates/snippets.clj
@@ -7,11 +7,17 @@
   (HTML/defsnippet ribbon "templates/snippets.html" [:.ribbon-area] []
     [:.datasource]
     (HTML/content (str "Ranoraraku: " (nth (DB/dbcp :ranoraraku-db) 1) ", Koteriku: " (nth (DB/dbcp :koteriku-db) 1))))
-  )
 
 (HTML/defsnippet head "templates/head.html" [:head] [title & [myscript]]
   [:title] (HTML/content title)
   [:#myscript] (when myscript (HTML/set-attr :src myscript)))
 
 (HTML/defsnippet ribbon "templates/ribbon.html" [:.ribbon-area] []
+  identity)
+  )
+
+(HTML/defsnippet head "templates/snippets.html" [:head] []
+  identity)
+
+(HTML/defsnippet scripts "templates/snippets.html" [:.scripts] []
   identity)
--- src/main/clojure/harborview/webapp.clj
+++ src/main/clojure/harborview/webapp.clj
@@ -2,11 +2,7 @@
   (:require
     [net.cgrand.enlive-html :as HTML]
     [compojure.route :as R]
-    [harborview.templates.snippets :as SNIP]
-    [harborview.elements.html :as E]
-    [harborview.systems.html :as SYS]
-    [harborview.nodes.html :as N]
-    [harborview.loads.html :as LD])
+    [harborview.templates.snippets :as SNIP])
   (:use
    [compojure.handler :only (api)]
    [compojure.core :only (GET defroutes context)]
@@ -14,8 +10,11 @@
    [ring.middleware.params :only (wrap-params)]))
 
 (HTML/deftemplate index "templates/index.html" []
-  [:head] (HTML/substitute (SNIP/head "Harbor View"))
-  [:.ribbon-area] (HTML/substitute (SNIP/ribbon)))
+  [:head] (HTML/substitute (SNIP/head))
+  [:.scripts] (HTML/substitute (SNIP/scripts)))
+
+  ;[:head] (HTML/substitute (SNIP/head "Harbor View"))
+  ;[:.ribbon-area] (HTML/substitute (SNIP/ribbon)))
 
 (defroutes main-routes
   (GET "/" request (index))
