ff56035bdc88e3995843b5d6a8a1e93c4d0d4733
rcs <rolf.c.stadheim@gmail.com>
2016-05-16 22:51:05 +0200

    Hourlist cleanup

--- src/harborview/hourlist/html.clj
+++ src/harborview/hourlist/html.clj
@@ -10,47 +10,6 @@
    [harborview.templates.snippets :as SNIP]))
 
 
-(comment H/deftemplate hourlist "templates/hourlist.html" [ctx]
-  [:head] (H/substitute (SNIP/head "Timeliste" "/js/hourlist.js"))
-  [:#fnr]
-  (UTIL/populate-select
-    (map (fn [v]
-           (let [fnr (.getInvoiceNum v)
-                 cust (.getCustomerName v)
-                 desc (.getDescription v)]
-             {:name (str fnr " - " cust " - " desc) :value (str fnr)}))
-      (DBX/fetch-invoices)))
-  [:#group]
-  (UTIL/populate-select
-    (map (fn [v]
-           {:name (str (.getId v) " - " (.getDescription v)) :value (str (.getId v))})
-      (DBX/fetch-hourlist-groups)))
-  [:p#message] (H/content (:message ctx)))
-
-(comment H/defsnippet groupsums "templates/snippets.html" [:.groupsums] [fnr]
-  [[:tr (H/attr= :class "rows")]]
-    (H/substitute
-      (map (fn [^HourlistGroupBean x]
-             {:tag :tr, :content [
-               {:tag :td, :content (.getDescription x)}
-               {:tag :td, :content (str (.getSumHours x))}]})
-        (DBX/fetch-group-sums fnr))))
-
-(comment H/defsnippet overview "templates/snippets.html" [:.overview] [fnr sel-fn]
-  [[:tr (H/attr= :class "rows")]]
-    (H/substitute
-      (map (fn [^HourlistBean x]
-              {:tag :tr, :content [
-                {:tag :td, :content (str (.getOid x))}
-                {:tag :td, :content (.getDescription x)}
-                {:tag :td, :content (str (.getInvoiceNr x))}
-                {:tag :td, :content (UTIL/date->str (.getLocalDate x))}
-                {:tag :td, :content (str (.getHours x))}
-                {:tag :td, :content (.getFromTime x)}
-                {:tag :td, :content (.getToTime x)}
-              ]})
-        (sel-fn fnr))))
-
 (defn hourlist[]
   (P/render-file "templates/hourlist/hourlist.html"
     {:invoices
@@ -78,21 +37,6 @@
             :totime (.getToTime x)})
       (select-fn fnr))}))
 
-
-(comment H/defsnippet overview "templates/snippets.html" [:.overview] [fnr sel-fn]
-                      [[:tr (H/attr= :class "rows")]]
-                      (H/substitute
-                        (map (fn [^HourlistBean x]
-                               {:tag :tr, :content [
-                                                     {:tag :td, :content (str (.getOid x))}
-                                                     {:tag :td, :content (.getDescription x)}
-                                                     {:tag :td, :content (str (.getInvoiceNr x))}
-                                                     {:tag :td, :content (UTIL/date->str (.getLocalDate x))}
-                                                     {:tag :td, :content (str (.getHours x))}
-                                                     {:tag :td, :content (.getFromTime x)}
-                                                     {:tag :td, :content (.getToTime x)}
-                                                     ]})
-                          (sel-fn fnr))))
 
 (defn hourlist[]
   (P/render-file "templates/hourlist/hourlist.html"
@@ -131,22 +75,6 @@
 
 (defroutes my-routes
   (GET "/" request (hourlist))
-  ;(GET "/" request (hourlist {:message "Beginning"})))
-  ;(GET "/groupsums" [fnr] (H/emit* (groupsums fnr)))
-  ;(GET "/overview" [fnr] (H/emit* (overview fnr DBX/fetch-all)))
-  (GET "/groupsums" [fnr] (groupsums fnr))
-  (GET "/overview" [fnr] (overview fnr DBX/fetch-all))
-  (PUT "/insert" [fnr group curdate from_time to_time hours]
-                 (do
-                   (DBX/insert-hourlist fnr group curdate from_time to_time hours)
-                   (overview fnr DBX/fetch-last-5))))
-
-;(U/json-response {"
-(defroutes my-routes
-  (GET "/" request (hourlist))
-  ;(GET "/" request (hourlist {:message "Beginning"})))
-  ;(GET "/groupsums" [fnr] (H/emit* (groupsums fnr)))
-  ;(GET "/overview" [fnr] (H/emit* (overview fnr DBX/fetch-all)))
   (GET "/groupsums" [fnr] (groupsums fnr))
   (GET "/overview" [fnr] (overview fnr DBX/fetch-all))
   (PUT "/insert" [fnr group curdate from_time to_time hours]
