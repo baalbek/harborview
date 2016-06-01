04f927a7df6bc4f9663ef1103f146f353d00f448
rcs <rolf.c.stadheim@gmail.com>
2016-06-01 23:58:28 +0200

    Logo, new acc/deny in purchase area like href-button

---
+++ src/harborview/xwebapp.clj
@@ -0,0 +1,38 @@
+(ns harborview.webapp
+  (:require
+   [selmer.parser :as P]
+   [compojure.route :as R]
+   [harborview.service.htmlutils :as U]
+   [harborview.critters.dbx :as DBX]
+   [harborview.hourlist.html :as HRL]
+   [harborview.critters.html :as CRT]
+   [harborview.derivatives.html :as OPX]
+   [harborview.generaljournal.html :as GJ]
+   [harborview.service.htmlutils :as UTIL])
+  (:use
+   [compojure.handler :only (api)]
+   [compojure.core :only (GET defroutes context)]
+   [ring.adapter.jetty :only (run-jetty)]
+   [ring.middleware.params :only (wrap-params)]))
+
+(P/set-resource-path! "/home/rcs/opt/java/harborview/src/resources/")
+(P/cache-off!)
+
+(defroutes main-routes
+  (GET "/" request (HRL/hourlist))
+  ;(GET "/" request (CRT/overlook (DBX/active-purchases (U/rs 11))))
+  (context "/generaljournal" [] GJ/my-routes)
+  (context "/hourlist" [] HRL/my-routes)
+  (context "/critters" [] CRT/my-routes)
+  (context "/opx" [] OPX/my-routes)
+  (R/files "/" {:root "public"})
+  (R/resources "/" {:root "public"}))
+
+(def webapp
+  (-> main-routes
+    api
+    wrap-params))
+
+;(def server (run-jetty #'webapp {:port 8082 :join? false}))
+
+(def server (run-jetty #'webapp {:port 8443 :join? false :ssl? true :keystore "keystore" :key-password "q2uebec9"}))
