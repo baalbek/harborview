311b674544c035f04ae4d912089c74a399809cce
rcs <rolf.c.stadheim@gmail.com>
2016-10-28 00:14:27 +0200

    Three.elm demo

--- elm/src/three.elm
+++ elm/src/three.elm
@@ -1,5 +1,7 @@
 
+import Html exposing (Html,div,text,select,option,text,p)
 import Html.App as App
+import Html.Attributes as HA
 
 main =
   App.program
@@ -9,3 +11,28 @@ main =
     , subscriptions = subscriptions
     }
 
+type alias Model = 
+    { yax : Int }
+
+type Msg
+    = Jada  
+    | Neida 
+
+init : ( Model, Cmd Msg )
+init = ( Model 5, Cmd.none )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model = ( model, Cmd.none )
+
+view : Model -> Html Msg
+view model =
+    div []
+       [ select [ HA.size model.yax ]
+            [ option [] [ text "1 youtertertj" ]
+            , option [] [ text "2 s32r23fwrf" ]
+            , option [] [ text "3 w23423sdffdsfs" ]
+            ]
+     ]
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
