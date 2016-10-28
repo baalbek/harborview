d454c6222ca586a50ee00e82558adecfef9b686c
rcs <rolf.c.stadheim@gmail.com>
2016-10-28 15:16:41 +0200

    More three combo

--- elm/src/three.elm
+++ elm/src/three.elm
@@ -1,4 +1,6 @@
 
+import Json.Decode as JD
+import Html.Events as HE
 import Html exposing (Html,div,text,select,option,text,p)
 import Html.App as App
 import Html.Attributes as HA
@@ -24,10 +26,20 @@ init = ( Model 5, Cmd.none )
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = ( model, Cmd.none )
 
+targetSelectedIndex = JD.at ["target", "selectedIndex"] JD.int
+
 view : Model -> Html Msg
 view model =
+  let
+    selectEvent = HE.on "change" targetSelectedIndex
+                  (S.message address << Select)
+  in
     div []
-       [ select [ HA.size 1 ]
+       [ select [ HA.class "form-control"
+                , HA.size 1
+         -- ,HA.style [ ("visibility", "hidden") ]     
+       ]
+
             [ option [] [ text "1 youtertertj" ]
             , option [] [ text "2 s32r23fwrf" ]
             , option [] [ text "3 w23423sdffdsfs" ]
