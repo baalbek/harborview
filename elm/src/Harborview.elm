9327e3c1df5ac08cf2362d64da7adf27dd73ea44
rcs <rolf.c.stadheim@gmail.com>
2016-11-09 00:08:58 +0100

    makeSelectOption

--- elm/src/Harborview.elm
+++ elm/src/Harborview.elm
@@ -3,7 +3,7 @@ module Harborview exposing (..)
 import Html as H 
 import Html.Attributes as A 
 import Html.App as App
-import Html.Events exposing (on) 
+import Html.Events as E 
 import Debug
 import Json.Decode as Json
 import VirtualDom as VD
@@ -30,8 +30,8 @@ type alias Model = Int
 
 
 type Msg
-    = NoOp
-    | TimeUpdate Float
+  = Project String
+    | Location String
 
 
 -- INIT
@@ -70,13 +70,21 @@ view model =
     [
         H.div [ A.class "row" ]
         [
-            makeSelect "Projects: "
-            , makeSelect "Locations: "
+            makeSelect "Projects: " Project
+            , makeSelect "Locations: " Location
         ]
     ]
 
-makeSelect : String -> VD.Node a
-makeSelect caption = 
+makeSelectOption : (String,String) -> VD.Node a
+makeSelectOption (value, displayValue) = 
+      H.option
+        [ A.value value
+        -- , A.selected (lang == favorite2)
+        ]
+        [ H.text displayValue ]
+
+makeSelect : String -> (String -> a) -> VD.Node a
+makeSelect caption msg = 
     H.div [ A.class "col-sm-4"]
     [ 
         H.span []
@@ -84,13 +92,20 @@ makeSelect caption =
             H.label [] [ H.text caption ]
             , H.select
             [   
-                -- onChange SelectFavorite
-                A.class "form-control"
+                onChange msg 
+                , A.class "form-control"
             ]
             []
             -- (List.map selectListOptions <| Dict.toList model.languages)
         ]
     ]
+
+onChange : (String -> a) -> VD.Property a 
+onChange tagger =
+  E.on "change" (Json.map tagger E.targetValue)
+
+(>>=) : Maybe a -> (a -> Maybe b) -> Maybe b
+(>>=) = Maybe.andThen 
 
 {--
 -- Custom event handler
