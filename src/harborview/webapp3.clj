a2f9a9d7ebe28ac8b8736e20c19b4c24ea45cd3e
rcs <rolf.c.stadheim@gmail.com>
2016-11-17 15:47:07 +0100

    webapp3 with selmer

--- src/harborview/webapp3.clj
+++ src/harborview/webapp3.clj
@@ -1,5 +1,6 @@
 (ns harborview.webapp3
   (:require
+    [selmer.parser :as P]
     [clj-json.core :as json])
   (:use
    [compojure.handler :only (api)]
@@ -7,21 +8,30 @@
    [ring.adapter.jetty :only (run-jetty)]
    [ring.middleware.params :only (wrap-params)]))
 
+(P/set-resource-path! "/home/rcs/opt/java/harborview/src/resources/")
+(P/cache-off!)
+
 (defn json-response [data & [status]]
   {:status (or status 200)
    :headers {"Content-Type" "application/json"}
    :body (json/generate-string data)})
 
-(defn projects []
-  (json-response 
+(defn fetch-projects-1 []
     [
      {:v 1 :t "NEW: Oid 1"}
      {:v 2 :t "NEW: Oid 2"}
      {:v 3 :t "NEW: Oid 3"}
-     ]))
-  ;(json-response {"oid" "Success! 3"}))
+     ])
+
+(defn projects []
+  (P/render-file "templates/vinapu/projects.html" {}))
+
+(defn fetch-projects []
+  (json-response 
+    (fetch-projects-1)))
 
 (defroutes main-routes
+  (GET "/vinapu/projects" [] (fetch-projects))
   (GET "/" request (projects)))
 
 (def webapp
@@ -29,5 +39,5 @@
     api
     wrap-params))
 
-(def server (run-jetty #'webapp {:port 8082 :join? false}))
-;(def server (run-jetty #'webapp {:port 8443 :join? false :ssl? true :keystore "keystore" :key-password "q2uebec9"}))
+;(def server (run-jetty #'webapp {:port 8082 :join? false}))
+(def server (run-jetty #'webapp {:port 8443 :join? false :ssl? true :keystore "keystore" :key-password "q2uebec9"}))
