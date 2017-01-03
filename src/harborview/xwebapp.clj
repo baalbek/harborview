feac5deb30dfdc9ea3c6aadd8dd0ac478af965e9
rcs <rolf.c.stadheim@gmail.com>
2017-01-03 15:41:28 +0100

    Stealth menu

--- src/harborview/xwebapp.clj
+++ src/harborview/xwebapp.clj
@@ -21,8 +21,8 @@
 (P/cache-off!)
 
 (defroutes main-routes
-  ;(GET "/" request (VIN/projects))
-  (GET "/" request (MAU/init))
+  (GET "/" request (VIN/projects))
+  ;(GET "/" request (MAU/init))
   ;(GET "/" request (HRL/hourlist))
   ;(GET "/" request (CRT/overlook (DBX/active-purchases (U/rs 11))))
   (context "/generaljournal" [] GJ/my-routes)
