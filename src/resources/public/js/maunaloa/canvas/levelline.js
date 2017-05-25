9df7067ece741fcab3a6651e95cc734582f68875
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2017-05-25 23:21:29 +0200

    Red marker on nearest line

--- src/resources/public/js/maunaloa/canvas/levelline.js
+++ src/resources/public/js/maunaloa/canvas/levelline.js
@@ -1,15 +1,38 @@
 var MAUNALOA = MAUNALOA || {};
 
+/*
+MAUNALOA.draggable = {
+  lerp : function(a,b,x){
+    return(a+x*(b-a));
+  },
+  shortestDistance : function(mx,my) {
+    var dx=this.x2-this.x1;
+    var dy=this.y2-this.y1;
+    var t=((mx-this.x1)*dx+(my-this.y1)*dy)/(dx*dx+dy*dy);
+    t=Math.max(0,Math.min(1,t));
+    var x=this.lerp(this.x1,this.x2,t);
+    var y=this.lerp(this.y1,this.y2,t);
+    return({x:x,y:y});
+  },
+};
+*/
+
 MAUNALOA.levelLine = {
     color : "red",
+    draggable : true,
+    legend : function() {
+      return this.levelValue;
+    },
     draw : function(repos) {
+        var y = this.y1;
         var ctx = repos.ctx;
-        console.log(ctx + "x1: " + this.x1 + ", y: " + this.y);
         ctx.beginPath();
-        ctx.moveTo(this.x1,this.y);
-        ctx.lineTo(this.x2,this.y);
+        ctx.moveTo(this.x1,y);
+        ctx.lineTo(this.x2,y);
         ctx.strokeStyle=this.color;
         ctx.stroke();
+        ctx.fillText(this.legend(),this.x1,y-10);
+        //ctx.fillText("[" + this.id + "] " + this.levelValue,this.x1,this.y-10);
     }
     /*
     create : function(levelValue) {
@@ -17,6 +40,7 @@ MAUNALOA.levelLine = {
     }
     */
 };
+
 
 // https://stackoverflow.com/questions/9880279/how-do-i-add-a-simple-onclick-event-handler-to-a-canvas-element
 
@@ -32,7 +56,8 @@ MAUNALOA.repos = {
     this.offsetY = clientRect.top;
     */
     // listen for mouse events
-    this.canvas.addEventListener('mouseup', this.handleMouseUpOut(this), false);
+    this.canvas.addEventListener('mouseup', this.handleMouseUp(this), false);
+    this.canvas.addEventListener('mouseout', this.handleMouseOut(this), false);
     this.canvas.addEventListener('mousedown', this.handleMouseDown(this), false);
     this.canvas.addEventListener('mousemove', this.handleMouseMove(this), false);
     //this.canvas.mousedown(function(e){handleMouseDown(e);});
@@ -40,19 +65,30 @@ MAUNALOA.repos = {
     //this.canvas.mouseup(this.handleMouseUpOut(this));
     //this.canvas.mouseout(function(e){handleMouseUpOut(e);});
   },
-  handleMouseUpOut : function(self) {
+  handleMouseOut : function(self) {
+    return function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  },
+  handleMouseUp : function(self) {
     return function(e) {
       // tell the browser we're handling this event
       e.preventDefault();
       e.stopPropagation();
-      self.draw();
     }
   },
+  isDown : false,
+  nearest : null,
   handleMouseDown : function(self) {
     return function(e) {
       // tell the browser we're handling this event
       e.preventDefault();
       e.stopPropagation();
+      self.nearest=self.closestLine(e.offsetX,e.offsetY);
+      self.draw();
+      // set dragging flag
+      self.isDown=true;
     }
   },
   handleMouseMove : function(self) {
@@ -60,37 +96,37 @@ MAUNALOA.repos = {
       // tell the browser we're handling this event
       e.preventDefault();
       e.stopPropagation();
-      console.log(e.offsetX);
     }
   },
   lines : [],
-  draw : function() {
-    var len = this.lines.length;
-    for (var i=0; i<len; ++i) {
-        this.lines[i].draw(this);
-    }
+  // linear interpolation -- needed in setClosestLine()
+  lerp : function(a,b,x){
+    return(a+x*(b-a));
   },
-  create : function(canvasId,vruler) {
-    var result = Object.create(MAUNALOA.repos);
-    result.init(canvasId,vruler);
-    return result;
+  // find closest XY on line to mouse XY
+  closestXY : function(line,mx,my){
+    var x0=line.x1;
+    var y0=line.y1;
+    var x1=line.x2;
+    var y1=line.y2;
+    var dx=x1-x0;
+    var dy=y1-y0;
+    var t=((mx-x0)*dx+(my-y0)*dy)/(dx*dx+dy*dy);
+    t=Math.max(0,Math.min(1,t));
+    var x=this.lerp(x0,x1,t);
+    var y=this.lerp(y0,y1,t);
+    return({x:x,y:y});
   },
-  addLevelLine : function(lineId,levelValue) {
-    var result = Object.create(MAUNALOA.levelLine);
-    result.x1 = 20;
-    result.x2 = this.canvas.width;
-    result.y = this.vruler.valueToPix(levelValue);
-    this.lines.push(result);
-  }
-}
-
-MAUNALOA.draggable = {
   // select the nearest line to the mouse
   closestLine : function (mx,my,lines){
+    var len = this.lines.length;
+    if (len === 0) {
+      return null;
+    }
     var dist=100000000;
     var index,pt;
-    for(var i=0;i<lines.length;i++){
-        var xy=closestXY(lines[i],mx,my);
+    for(var i=0;i<len;i++){
+        var xy=this.closestXY(this.lines[i],mx,my);
         var dx=mx-xy.x;
         var dy=my-xy.y;
         var thisDist=dx*dx+dy*dy;
@@ -100,11 +136,59 @@ MAUNALOA.draggable = {
             index=i;
         }
     }
-    var line=lines[index];
-    return({ pt:pt, line:line, originalLine:{x0:line.x0,y0:line.y0,x1:line.x1,y1:line.y1} });
+    var line=this.lines[index];
+    return({ pt:pt, line:line, originalLine:{x0:line.x1,y0:line.y1,x1:line.x2,y1:line.y2} });
+  },
+  draw : function() {
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    var len = this.lines.length;
+    for (var i=0; i<len; ++i) {
+        this.lines[i].draw(this);
+    }
+    // draw markers if a line is being dragged
+    if(this.nearest){
+        // point on line nearest to mouse
+        this.ctx.beginPath();
+        this.ctx.arc(this.nearest.pt.x,this.nearest.pt.y,5,0,Math.PI*2);
+        this.ctx.strokeStyle='red';
+        this.ctx.stroke();
+        /*
+        // marker for original line before dragging
+        drawLine(nearest.originalLine,'red');
+        // hightlight the line as its dragged
+        drawLine(nearest.line,'red');
+        */
+    }
+  },
+  create : function(canvasId,vruler) {
+    var result = Object.create(MAUNALOA.repos);
+    result.init(canvasId,vruler);
+    return result;
+  },
+  addLevelLine : function(lineId,levelValue) {
+    /*
+    var result = Object.create(MAUNALOA.levelLine, {
+                                  y1 : {
+                                          get: function() {return this.y;},
+                                          set: function(value) {this.y=value;},
+                                        },
+                                  y2 : {
+                                          get: function() {return this.y;},
+                                          set: function(value) {this.y=value;},
+                                        },
+    */
+    var result = Object.create(MAUNALOA.levelLine);
+    result.id = lineId;
+    result.levelValue = levelValue;
+    result.x1 = 20;
+    result.x2 = this.canvas.width;
+    result.y1 = this.vruler.valueToPix(levelValue);
+    result.y2 = result.y1;
+    this.lines.push(result);
+    this.draw();
   }
+}
 
-};
 
 MAUNALOA.vruler = function(chartInfo) {
   var double2decimal = function(x,roundingFactor) {
