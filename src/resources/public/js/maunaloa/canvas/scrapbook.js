e519b45bb7bf6ca1910a29d6e70944e09de9de97
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2017-12-20 16:23:56 +0100

    Clear scrapbook button, impl draw single line array

--- src/resources/public/js/maunaloa/canvas/scrapbook.js
+++ src/resources/public/js/maunaloa/canvas/scrapbook.js
@@ -1,23 +1,21 @@
 var MAUNALOA = MAUNALOA || {};
 
 MAUNALOA.scrapbook = {
-  /*
-  create : function() {
-    var F = function() {
-    }
-    F.prototype = MAUNALOA.scrapbook;
-    F.constructor.prototype = F;
-    var result = new F();
-    return result;
-  },
-  */
   paint : false,
+  clickX : [],
+  clickY : [],
+  clixX : [],
+  clixY : [],
+  ctx : null,
   init : function() {
     var scrapbook = document.getElementById("scrapbook");
     if (scrapbook !== null) {
 
       var c_scrap = document.getElementById(MAUNALOA.repos.DAY_LINES_OVERLAY_2);
       c_scrap.addEventListener('mousedown', this.handleMouseDown(this), false);
+      c_scrap.addEventListener('mousemove', this.handleMouseMove(this), false);
+      c_scrap.addEventListener('mouseup', this.handleMouseUp(this), false);
+      this.ctx = c_scrap.getContext("2d"); 
 
       scrapbook.onchange = function() {
         var div_1x = document.getElementById("div-1x");
@@ -32,10 +30,58 @@ MAUNALOA.scrapbook = {
         }
       }
     }
+    var clearBtn = document.getElementById("btn-scrapbook-clear");
+    if (clearBtn !== null) {
+      var self = this;
+      clearBtn.onclick = function() {
+        var canvas = self.ctx.canvas;
+        self.ctx.clearRect(0, 0, canvas.width, canvas.height)
+        self.clickX = [];
+        self.clickY = [];
+        
+      }
+    }
+  },
+  addClick : function(x,y) {
+    this.clickX.push(x);
+    this.clickY.push(y);
+  },
+  redraw : function() {
+      var context = this.ctx;
+      var canvas = this.ctx.canvas;
+      //context.clearRect(0, 0, canvas.width, canvas.height)
+      context.strokeStyle = "#df4b26";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+      var cx = this.clickX; 
+      var cy = this.clickY; 
+        context.beginPath();
+        context.moveTo(cx[0], cy[0]);
+        for(var i=1; i < cx.length; ++i) {      
+            context.lineTo(cx[i], cy[i]);
+        }
+
+    //context.closePath();
+    context.stroke();
   },
   handleMouseDown : function(self) {
     return function(e) {
-        console.log(self.paint);
+        self.paint = true;
+        self.addClick(e.offsetX,e.offsetY);
+        self.redraw(e);
+    }
+  },
+  handleMouseMove : function(self) {
+    return function(e) {
+        if(self.paint){
+            self.addClick(e.offsetX,e.offsetY);
+            self.redraw();
+        }
+    }
+  },
+  handleMouseUp : function(self) {
+    return function(e) {
+        self.paint = false;
     }
   }
 }
