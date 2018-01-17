b70566715750699d8a80a3f38f434420728daf06
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2018-01-17 23:14:19 +0100

    50 pct fibonacci line

--- demo/svg_examples/svg1.js
+++ demo/svg_examples/svg1.js
@@ -1,48 +1,63 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  var svgLine = function(x1, y1, x2, y2) {
+    var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    l.setAttribute("x1", x1);
+    l.setAttribute("y1", y1);
+    l.setAttribute("x2", x2);
+    l.setAttribute("y2", y2);
+    l.setAttribute("stroke", "red");
+    l.setAttribute("stroke-width", 1);
+    return l;
+  }
   var addLine = function() {
     var curMarker = null;
     var curMarkerDown = function(e) {
       curMarker = e.target;
-      console.log(curMarker.id);
     }
     var curMarkerUp = function() {
-      console.log(curMarker.id);
       curMarker = null;
     }
-    var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    l.setAttribute("x1", "20");
-    l.setAttribute("y1", "20");
-    l.setAttribute("x2", "100");
-    l.setAttribute("y2", "100");
-    l.setAttribute("stroke", "red");
-    l.setAttribute("stroke-width", 2);
+    var l = svgLine("200", "20", "20", "200");
+    var l50 = svgLine("110", "110", "1200", "110");
     var c1 = draggableMarker("1", l.getAttribute("x1"), l.getAttribute("y1"), curMarkerDown, curMarkerUp);
     var c2 = draggableMarker("2", l.getAttribute("x2"), l.getAttribute("y2"), curMarkerDown, curMarkerUp);
 
     var svg = document.getElementById("svg1");
     svg.addEventListener("mousemove", function(e) {
       if (curMarker !== null) {
-        curMarker.setAttribute("cx", e.offsetX);
-        curMarker.setAttribute("cy", e.offsetY);
-        console.log(curMarker.id);
+        var x = e.offsetX;
+        var y = e.offsetY;
+        curMarker.setAttribute("cx", x);
+        curMarker.setAttribute("cy", y);
         if (curMarker.id === "1") {
-          l.setAttribute("x1", e.offsetX);
-          l.setAttribute("y1", e.offsetY);
+          l.setAttribute("x1", x);
+          l.setAttribute("y1", y);
+          var y0 = l.getAttribute("y2");
+          var x0 = l.getAttribute("x2");
         } else {
-          l.setAttribute("x2", e.offsetX);
-          l.setAttribute("y2", e.offsetY);
+          l.setAttribute("x2", x);
+          l.setAttribute("y2", y);
+          var y0 = l.getAttribute("y1");
+          var x0 = l.getAttribute("x1");
         }
+
+        var x50 = (Math.abs(x - x0) / 2.0) + Math.min(x, x0);
+        var y50 = (Math.abs(y - y0) / 2.0) + Math.min(y, y0);
+        l50.setAttribute("x1", x50);
+        l50.setAttribute("y1", y50);
+        l50.setAttribute("y2", y50);
       }
     });
     svg.appendChild(l);
+    svg.appendChild(l50);
     svg.appendChild(c1);
     svg.appendChild(c2);
-    drawCanvasLine();
   };
   var btn = document.getElementById('newline');
   btn.onclick = addLine;
 
   drawCanvasRect();
+  drawCanvasLine();
 
 });
 
@@ -96,7 +111,7 @@ var drawCanvasLine = function() {
   var ctx = canvas.getContext("2d");
   ctx.strokeStyle = "red";
   ctx.beginPath();
-  ctx.moveTo(105, 105);
+  ctx.moveTo(110, 110);
   ctx.lineTo(200, 200);
   ctx.stroke();
 }
